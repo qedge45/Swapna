68d4e10971db3fd311e6d6dc8ab16a3378c45b29
Sahasra <Sahasra@192.168.0.16>
2020-05-19 20:32:08 -0400

    New class added

---
+++ Selenium/src/com/selenium/java/GithubTest.java
@@ -0,0 +1,12 @@
+package com.selenium.java;
+
+public class GithubTest {
+
+	public static void main(String[] args)
+	{
+		// Adding new class to Github
+		System.out.println("New class added to Github");
+
+	}
+
+}
